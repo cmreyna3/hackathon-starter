1e4282c0eadc703e234a179159e7226a37ae0935
Christopher Reyna <cmreyna3@gmail.com>
2021-05-04 20:25:44 -0600

    updated ip in deploy.js

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.226.18.157',
+      host: '54.221.1.115',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
